f3201f38f7ea3105ff8c32aa7f6cc502988eec43
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-04-20 01:04:41 +0530

    update0.2

--- untitled/src/app.js
+++ untitled/src/app.js
@@ -3,8 +3,11 @@ const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 3500
-
 const publicStaticDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.use(express.static(publicStaticDirPath));
 
  app.get('',(req,res) => {
     res.send("hiii hutto")
---
+++ untitled/utils/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q= ",
+        SECRET_KEY: "4ab9750d9f42b86c5adbb5d764b36ac5"
+    }
+}
+
+module.exports = constants ;
