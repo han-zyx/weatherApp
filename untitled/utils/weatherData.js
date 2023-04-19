c9ea402bad6fb030a5a9f8298bad4e00f090ec16
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-04-20 02:00:52 +0530

    update0.4

--- untitled/utils/weatherData.js
+++ untitled/utils/weatherData.js
@@ -1,8 +1,20 @@
-const constants = require('../utils/config');
+const request = require('request');
+const constant = require('../utils/config');
+const {openWeatherMap} = require("./config");
 
 const weatherData = (address , callback) => {
-    const url = constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.SECRET_KEY;
+    const url = openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + openWeatherMap.SECRET_KEY;
     console.log(url);
+    request({url,json:true}, (error , {body}) => {
+        console.log(body);
+        if(error){
+            callback("Can't Get Data" , undefined)
+        } else {
+            callback(undefined,{
+                abc : true
+            })
+        }
+    })
     callback(true);
 }
 
