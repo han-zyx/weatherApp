eb155052f6ce289697421c71090430da100a2498
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-04-30 14:06:12 +0530

    update0.7

--- untitled/public/js/app_fr.js
+++ untitled/public/js/app_fr.js
@@ -23,4 +23,31 @@ dateElemnt.textContent = monthName[new Date().getMonth()] +" "+ new Date().getDa
 weatherForm.addEventListener('submit',(event) => {
     event.preventDefault();
     console.log(search.value);
+
+    locationElement.textContent = "මොකද හදිස්සි , පොඩ්ඩක් ඉවසන්න ...";
+    tempElement.textContent = "";
+    weatherCondition.textContent = "";
+
+    const locationApi =  fetchWeather + "?address=" + search.value;
+    fetch(locationApi).then(response => {
+        response.json().then(data => {
+            if(data.error){
+                locationElement.textContent = "අනේ හරියට ඉන්න තැන ටයිප් කරපන්කො.";
+                tempElement.textContent = "";
+                weatherCondition.textContent = "";
+
+            } else {
+               /* if (data.description === "rain" || data.description === "fog"){
+                    weatherCondition.className = "wi wi-day-" + data.description
+                } else {
+                    weatherIcon.className = "wi wi-day-cloudy"
+                } */
+                locationElement.textContent = data.cityName;
+                tempElement.textContent = (data.temperature - 273.5).toFixed(2) + String.fromCharCode(176);
+                weatherCondition.textContent = data.description.toUpperCase();
+            }
+        })
+    })
+
+
 })
