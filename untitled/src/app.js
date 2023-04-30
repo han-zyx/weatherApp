0d661aa59d53c8cd7f358217274f7cef30722949
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-04-30 12:01:01 +0530

    update0.5

--- untitled/src/app.js
+++ untitled/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 
 const port = process.env.PORT || 3500
 const publicStaticDirPath = path.join(__dirname, '../public');
@@ -8,19 +9,45 @@ const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
 const weatherData =  require('../utils/weatherData');
 
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
 app.use(express.static(publicStaticDirPath));
 
- app.get('',(req,res) => {
-    res.send("hiii hutto")
-})
 
-app.get('/weather',(req,res) => {
-    const address =  req.query.address
-
-    weatherData(address,(result) => {
-        console.log(result)
+/*
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App'
     })
+})
+*/
 
+app.get('', (req,res) => {
+    res.send("hii this is weather app")
+});
+
+app.get('/weather', (req, res) => {
+    const address = req.query.address
+    if(!address) {
+        return res.send({
+            error: "You must enter address in search text box"
+        })
+    }
+
+    weatherData(address, (error, {temperature, description, cityName} = {}) => {
+        if(error) {
+            return res.send({
+                error
+            })
+        }
+        console.log(temperature, description, cityName);
+        res.send({
+            temperature,
+            description,
+            cityName
+        })
+    })
 });
 
 app.get("*",(req, res ) => {
--- untitled/utils/weatherData.js
+++ untitled/utils/weatherData.js
@@ -4,18 +4,21 @@ const {openWeatherMap} = require("./config");
 
 const weatherData = (address , callback) => {
     const url = openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + openWeatherMap.SECRET_KEY;
-    console.log(url);
-    request({url,json:true}, (error , {body}) => {
-        console.log(body);
-        if(error){
-            callback("Can't Get Data" , undefined)
+
+    request({url, json:true}, (error, {body})=> {
+
+        if(error) {
+            callback("Can't fetch data from open weather map api ", undefined)
+        } else if(!body.main || !body.main.temp || !body.name || !body.weather) {
+            callback("Unable to find required data, try another location", undefined);
         } else {
-            callback(undefined,{
-                abc : true
+            callback(undefined, {
+                temperature: body.main.temp,
+                description: body.weather[0].description,
+                cityName: body.name
             })
         }
     })
-    callback(true);
 }
 
 module.exports = weatherData;
